73b063122d63b8dfb811460aa731fe86f37f540e
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-03-01 07:56:48 +0100

    Add 1.29

---
+++ sicp.clj
@@ -0,0 +1,16 @@
+(ns sicp)
+
+; exercice 1.29
+
+(defn sum [term a next b]
+  (if (> a b)
+    0
+    (+ (term a)
+       (sum term (next a) next b))))
+
+(defn simp-integral
+  [f a b n]
+  (let [h (/ (- b a) n)]
+    (letfn [(term [k] (* (f (+ a (* k h))) (if (even? k) 2 4)))]
+      (/ (* h (+ a (sum term 1 inc n)) ) 3) )))
+
