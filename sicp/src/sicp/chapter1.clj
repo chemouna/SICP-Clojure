5085c765707e49d8322054beebcd46cf4f9b2e55
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-03-16 09:40:18 +0000

    Fix run issue.

--- sicp/src/sicp/chapter1.clj
+++ sicp/src/sicp/chapter1.clj
@@ -1,7 +1,9 @@
 (ns sicp.chapter1)
 
+(defn abs [n] (max n (- n)))
+
 (defn good-enough? [guess x]
-  (< (Math/abs (- (* guess guess) x)) 0.001))
+  (< (abs (- (* guess guess) x)) 0.001))
 
 (defn average [x y]
   (/ (+ x y) 2))
@@ -18,10 +20,8 @@
 (defn sqrt [x]
   (sqrt-iter 1.0 x))
 
-;; (good-enough? 1 1.001)
-
-
 ;; Exercise 1.7
+
 
 ;; (sqrt 0.0001) yields 0.03230844833048122 instead of the expected 0.01 (an error of over 200%).
 
