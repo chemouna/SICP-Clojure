bf2cc61536d49032668bd0e964c86683ef4fe674
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-03-17 17:37:12 +0000

    Count change solution iterative process.

--- sicp/src/sicp/chapter1.clj
+++ sicp/src/sicp/chapter1.clj
@@ -39,5 +39,65 @@
 (defn cb [x]
   (cb-iter 1.0 0.0 x))
 
-(cb 2)
+;; count change
+
+;; recursive process version
+(defn first-denomination [kinds-of-coins]
+	(cond (= kinds-of-coins 1) 1
+        (= kinds-of-coins 2) 5
+        (= kinds-of-coins 3) 10
+        (= kinds-of-coins 4) 25
+        (= kinds-of-coins 5) 50))
+
+(defn cc [amount kinds-of-coins]
+	(cond (= amount 0) 1
+        (or (< amount 0) (= kinds-of-coins 0)) 0
+        :else (+ (cc (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins)
+                 (cc amount
+                     (- kinds-of-coins 1)))))
+
+(defn count-change [amount]
+	(cc amount 5))
+
+;; (count-change 100)
+
+;; an iterative process version
+
+(defn cc-nothing [amount acc]
+	acc)
+
+(defn cc-pennies [amount acc]
+	(cond (= amount 0) (+ 1 acc)
+        (< amount 0) acc
+        :else (cc-pennies (- amount 1)
+                          (cc-nothing amount acc))))
+(defn cc-nickels [amount acc]
+	(cond (= amount 0) (+ 1 acc)
+        (< amount 0) acc
+        :else (cc-nickels (- amount 5)
+                          (cc-pennies amount acc))))
+(defn cc-dimes [amount acc]
+	(cond (= amount 0) (+ 1 acc)
+        (< amount 0) acc
+        :else (cc-dimes (- amount 10)
+                        (cc-nickels amount acc))))
+
+(defn cc-quarters [amount acc]
+	(cond (= amount 0) (+ 1 acc)
+        (< amount 0) acc
+        :else (cc-quarters (- amount 25)
+                           (cc-dimes amount acc))))
+
+(defn cc-fifties [amount acc]
+	(cond (= amount 0) (+ 1 acc)
+        (< amount 0) acc
+        :else (cc-fifties (- amount 50)
+                          (cc-quarters amount acc))))
+
+(defn count-change-iter [amount]
+	(cc-fifties amount 0))
+
+;; (count-change-iter 100)
 
