48444c33ae9310ca00dd87d672fcdae980705b40
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-03-23 19:53:15 +0000

    ex 1.19

--- sicp/src/sicp/chapter1.clj
+++ sicp/src/sicp/chapter1.clj
@@ -218,4 +218,12 @@
   (cond (= count 0) b
         :else (fib-iter (+ a b) a (- count 1))))
 
+;; sol 1.19
+(defn fib-iter2 [a b p q count]
+  (cond (= count 0) b
+        (even? count) (fib-iter2 a b (+ (* p p) (* q q)) (+ (* 2 p q) (* q q)) (/ count 2)))
+         :else (fib-iter2 (+ (* b q) (* a q) (* a p)) (+ (* b p) (* a q)) p q (- count 1)))
+
+(defn fib3 [n]
+  (fib-iter2 1 0 0 1 n))
 
