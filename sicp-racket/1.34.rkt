423e3b2fcf2202b44679770e64a5f7bfa3d1280c
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-03-26 09:25:14 +0100

    ex 1.34

---
+++ sicp-racket/1.34.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+(define (square x) (* x x))
+
+(define (f g)
+  (g 2))
+
+;(f square)
+;(f (lambda (z) (* z (+ z 1))))
+;(f f)
+
+; (f f) we get an error: application: not a procedure; expected a procedure that can be applied to arguments
