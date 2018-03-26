c1fe0b39e7134dd460af1bf319b941640daddc23
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-03-26 08:43:24 +0100

    1.33 b/ in racket.

--- sicp-racket/1.33.rkt
+++ sicp-racket/1.33.rkt
@@ -10,16 +10,21 @@
   (if (> a b) null-value
       (combiner (term a) (filtered-accumulate combiner null-value term (new-next a) next b predicate))))
       
+(define (inc x) (+ x 1))
 
 ;; sum of squares of the prime numbers in the interval a to b
 (define (sum-squares-primes a b)
   (define (square x) (* x x))
-  (define (inc x) (+ x 1)) ;; this next can be improved given what we know about primes kx+v ....
   (filtered-accumulate + 0 square a inc b prime?))
 
-;; (trace filtered-accumulate)
-;; (trace sum-squares-primes)
+;(sum-squares-primes 1 2) ;; 1 + 4 = 5
+;(sum-squares-primes 1 3) ;; 1 + 4 + 9 = 14
 
-(sum-squares-primes 1 2) ;; 1 + 4 = 5
+;; product of all the positive integers less than n that are relatively prime to n
+;; (i.e., all positive integers i < n such that GCD(i,n) = 1).
+(define (product-pos-coprime n)
+  (define (identity x) x)
+  (define (coprime-n? x) (coprime? n x))
+  (filtered-accumulate * 1 identity 2 inc (- n 1) coprime-n?))
 
-(sum-squares-primes 1 3) ;; 1 + 4 + 9 = 14
+; (product-pos-coprime 7)
