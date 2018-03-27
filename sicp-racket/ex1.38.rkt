2ce0d9ae401ecebe3346eeea4f1ffda0e9c56027
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-03-27 01:42:21 +0100

    Sol to ex 1.38

--- sicp-racket/ex1.38.rkt
+++ sicp-racket/ex1.38.rkt
@@ -2,12 +2,17 @@
 
 (require racket/trace)
 
-(define (di i)
-  (cond ((or (= i 0) (= i 2) (= i 3) (= i 5) (= i 6)) 1)
-        ((= i 1) 2)
-        ((= i 4) 4)
-        ((= i 7) 6)
-        (- (* 2 (di (- i 3))) (di (- i 6)))))
+;(define (d2 i)
+;  (cond ((or (= i 0) (= i 2) (= i 3) (= i 5) (= i 6)) 1)
+;        ((= i 1) 2)
+;        ((= i 4) 4)
+;       ((= i 7) 6)
+;        (- (* 2 (d2 (- i 3))) (d2 (- i 6)))))
+
+(define (d i)
+   (if (not (= 0 (remainder (+ i 1) 3)))
+       1
+       (* 2 (/ (+ i 1) 3))))
 
 (define (cont-frac n d k)
   (define (frac i)
@@ -16,7 +21,4 @@
         (/ (n i) (d i))))
   (frac 1))
 
-(trace di)
-(trace cont-frac)
-
-(cont-frac (lambda (i) 1.0) di 10) 
+(+ 2 (cont-frac (lambda (i) 1.0) d 10)) 
