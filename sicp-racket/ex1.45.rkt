e93e2e0bdd92fe2dcfe44997f0c5557bb0f75704
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-03-27 19:56:40 +0100

    ex 1.45 in racket

---
+++ sicp-racket/ex1.45.rkt
@@ -0,0 +1,30 @@
+#lang racket
+
+(define (square x) (* x x))
+(define (cube x) (* x x x))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (sqrt x)
+  (fixed-point (average-damp (lambda (y) (/ x y))) 1.0))
+
+(define (cube-root x)
+  (fixed-point (average-damp (lambda (y) (/ x (square y)))) 1.0))
+
+(define (nth-root n x)
+  (fixed-point (average-damp (lambda (y) (/ x (expt y (- n 1))))) 1.0))
