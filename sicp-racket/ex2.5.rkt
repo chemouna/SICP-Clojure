4a9db2e94209344f25a8bcd46bf582718c602c55
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-03-28 20:19:17 +0100

    A better solution for 2.5 in racket

--- sicp-racket/ex2.5.rkt
+++ sicp-racket/ex2.5.rkt
@@ -2,32 +2,22 @@
 
 (require racket/trace)
 
-(define (iterative-improve is-good-enough? improve)
-  (define (try guess)
-     (if (is-good-enough? guess)
-         guess
-         (try (improve guess))))
-  (lambda (x) (try x)))
-
 (define (divides? a b) (= (remainder b a) 0))
+
+(define (get-exp x n m)
+         (if (divides? m x) (get-exp (/ x m) n m)
+             (exact-floor (log x n))))
 
 (define (cons a b)
   (* (expt 2 a) (expt 3 b)))
 
-(define (find-exp x n m)
-  ((iterative-improve
-   (lambda (guess) (and (integer? (log guess n)) (divides? n (log guess n))))
-   (lambda (guess) (/ guess m))) x))
-
-;; keep loging untill you get something that divides
 (define (car z)
-  (find-exp z 2 3))
+  (get-exp z 2 3))
 
 (define (cdr z)
-  (find-exp z 3 2))
+  (get-exp z 3 2))
 
-(trace car)
-(trace cons)
-(trace find-exp)
-(trace divides?)
-(trace iterative-improve)
+;; (trace cdr)
+;; (trace car)
+;; (trace cons)
+;; (trace get-exp)
