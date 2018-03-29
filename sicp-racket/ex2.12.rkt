3dbe08154fd662338c23f0383d5632e273b2799d
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-03-29 08:48:49 +0100

    ex 2.12 in racket

---
+++ sicp-racket/ex2.12.rkt
@@ -0,0 +1,28 @@
+#lang racket
+
+(define (make-interval x y)
+  (cons x y))
+
+(define (lower-bound i)
+  (car i))
+
+(define (upper-bound i)
+  (cdr i))
+
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+
+(define (make-center-percent c p)
+  (make-center-width c (* c (/ p 100.0))))
+
+(define (percent i)
+  (* (/ (width i) (center i)) 100.0))
+
+(define a (make-center-percent 5 20))
+(center a)
+(percent a)
