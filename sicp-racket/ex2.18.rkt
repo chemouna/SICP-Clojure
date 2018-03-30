7c6625e38f0f614b30bdf279c781c99f758ac442
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-03-30 18:58:34 +0100

    solution 2.18

--- sicp-racket/ex2.18.rkt
+++ sicp-racket/ex2.18.rkt
@@ -10,3 +10,15 @@
 (reverse (list 1 4 9 16 25))
 ;; (25 16 9 4 1)
 
+;; solution 2
+(define (append list1 list2)
+  (if (null? list1)
+      list2
+      (cons (car list1) (append (cdr list1) list2))))
+
+(define (reverse2 items)
+  (if (null? items)
+      items
+      (append (reverse2 (cdr items)) (list (car items)))))
+
+(reverse2 (list 1 4 9 16 25))
