8387a1d8e2451d3aaaf91ea9c5bb8ae8b3056a56
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-03-30 21:46:43 +0100

    Solution for ex 2.19

--- sicp-racket/ex2.19.rkt
+++ sicp-racket/ex2.19.rkt
@@ -2,37 +2,29 @@
 
 (require racket/trace)
 
-(define (length items)
-  (if (null? items)
-      0
-      (+ 1 (length (cdr items)))))
-(trace length)
+(define (first-denomination coins)
+  (car coins))
 
-(define (list-ref items n)
-  (if (= n 0) 
-      (car items)
-      (list-ref (cdr items) (- n 1))))
-(trace list-ref)
+(define (no-more? lst) (null? lst))
 
-(define (first-denomination coins k)
-  (list-ref coins k))
-(trace first-denomination)
-
-;; maybe we can get rid of the usage of k here 
-(define (cc-helper amount coins k)
+(define (cc amount coin-values)
   (cond ((= amount 0) 1)
-        ((or (< amount 0) (= k 0)) 0)
-        (+ (cc-helper (- amount (first-denomination coins k)) coins k)
-           (cc-helper amount coins (- k 1)))))
-(trace cc-helper)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+         (+ (cc amount (cdr coin-values))
+            (cc (- amount
+                   (first-denomination coin-values))
+                coin-values)))))
 
-(define (cc amount coins)
-  (cc-helper amount coins (- (length coins) 1)))
-(trace cc)
 
 (define us-coins (list 50 25 10 5 1))
 (define uk-coins (list 100 50 20 10 5 2 1 0.5))
 
+;(trace length)
+;(trace first-denomination)
+;(trace cc)
+
 (cc 100 us-coins)
+
 
 
