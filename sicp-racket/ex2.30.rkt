94bebfa11b8f863d94d73c800716b31edfc2cef8
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-03-31 22:57:01 +0100

    Sol 2.30

---
+++ sicp-racket/ex2.30.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(define nil '())
+
+(define (square-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (* tree tree))
+        (else (cons (square-tree (car tree))
+                    (square-tree (cdr tree))))))
+
+(square-tree
+ (list 1
+       (list 2 (list 3 4) 5)
+       (list 6 7)))
+
+(define (square-tree-2 tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (square-tree-2 sub-tree)
+             (* sub-tree sub-tree))) tree))
+
+(square-tree-2
+ (list 1
+       (list 2 (list 3 4) 5)
+       (list 6 7)))
