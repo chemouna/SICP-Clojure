e359545dc79badd7845a1a97d1bb8ba0255563fd
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-03-31 11:45:06 +0100

    2.25 - 2.26 solutions.

---
+++ sicp-racket/ex2.25-2.26.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(define (count-leaves x)
+  (cond ((null? x) 0)
+        ((not (pair? x)) 1)
+        (else (+ (count-leaves (car x))
+                 (count-leaves (cdr x))))))
+
+
+;; ex 2.25
+(car (cdr (car (cdr (cdr '(1 3 (5 7) 9))))))
+(car (car '((7))))
+(car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr '(1 (2 (3 (4 (5 (6 7))))))))))))))))))
+
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+(append x y) ;; '(1 2 3 4 5 6)
+(cons x y) ;; '((1 2 3) 4 5 6)
+(list x y) ;; '((1 2 3) (4 5 6))
