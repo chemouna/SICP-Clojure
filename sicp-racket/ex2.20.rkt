9283f75f1ac95a98252278b905d234c6a7e86ffd
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-03-31 10:01:39 +0100

    Working but ugly solution.

--- sicp-racket/ex2.20.rkt
+++ sicp-racket/ex2.20.rkt
@@ -1,7 +1,11 @@
 #lang racket
 
+(require racket/trace)
+
 (define (even? n)
    (= (remainder n 2) 0))
+
+(define (odd? n) (not (even? n)))
 
 (define (length items)
   (if (null? items)
@@ -15,21 +19,41 @@
 
 (define (simple-filter pred lst res)
   (cond ((null? lst) res)
-        ((= (length lst) 1) (append (car lst) res))
-        ((pred (car lst)) (simple-filter pred (cdr lst) (append (car lst) res)))
-        (simple-filter pred (cdr lst) res)))
+        ((pred (car lst)) (simple-filter pred (cdr lst) (append (list (car lst)) res)))
+        (else (simple-filter pred (cdr lst) res))))
 
 (define (only-even lst)
   (simple-filter even? lst '()))
 
 (define (only-odd lst)
-  (simple-filter even? lst '()))
+  (simple-filter odd? lst '()))
 
 (define (same-parity x . y)
-  (if (even? x) (only-even y)
-      (only-odd y)))
+  (if (even? x) (reverse (append (only-even y) (cons x '())))
+      (reverse (append (only-odd y) (cons x '())))))
+
+;(trace even?)
+;(trace odd?)
+;(trace length)
+;(trace simple-filter)
+;(trace only-even)
 
 (same-parity 1 2 3 4 5 6 7)
-; (1 3 5 7)
 (same-parity 2 3 4 5 6 7)
+
+;; solution 2
+(define (same-parity2 a . z)
+   (define (iter items answer)
+     (if (null? items)
+         answer
+         (iter (cdr items)
+               (if (= (remainder (car items) 2)
+                      (remainder a 2))
+                   (append answer (list (car items)))
+                   answer))))
+   (iter z (list a)))
+
+;(same-parity 1 2 3 4 5 6 7)
+; (1 3 5 7)
+;(same-parity 2 3 4 5 6 7)
 ; (2 4 6)
