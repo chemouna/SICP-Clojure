899571326fe063e56c6dc375a87543293880f7cb
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-03-31 10:52:59 +0100

    rest of section 2.2.1

--- sicp-racket/section2.2.1.rkt
+++ sicp-racket/section2.2.1.rkt
@@ -32,4 +32,26 @@
 (append squares odds)
 (append odds squares)
 
+;; map over lists
+(define nil '())
 
+(define (scale-list items factor)
+  (if (null? items)
+      nil
+      (cons (* (car items) factor)
+            (scale-list (cdr items) factor))))
+
+(scale-list (list 1 2 3 4 5) 10)
+
+(define (map proc items)
+  (if (null? items)
+      nil
+      (cons (proc (car items))
+            (map proc (cdr items)))))
+
+(map abs (list -10 2.5 -11.6 17))
+(map (lambda (x) (* x x))
+     (list 1 2 3 4))
+
+(define (scale-list2 items factor)
+  (map (lambda (x) (* x factor)) items))
