b804c16685f7ba25f33bd165aaaa6239b9944e42
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-04-01 23:17:02 +0100

    Solution to ex 2.42

---
+++ sicp-racket/ex2.42.rkt
@@ -0,0 +1,64 @@
+#lang racket
+
+(define nil '())
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (empty-board) '())
+
+(define (make-position row col)
+  (cons row col))
+
+(define (position-row p)
+  (car p))
+
+(define (position-col p)
+  (cdr p))
+
+(define (adjoin-position row col positions)
+  (cons (make-position row col) positions))
+
+(define (safe? col positions)
+  (let ((kth-queen (list-ref positions (- col 1)))
+        (other-queens (filter (lambda(q)
+                                (not (= (position-col q) col)))
+                              positions)))
+  (define (attacks? q1 q2)
+    (or (= (position-row q1) (position-row q2))
+        (= (position-col q1) (position-col q2))
+        (= (abs (- (position-row q1) (position-row q2)))
+           (abs (- (position-col q1) (position-col q2))))))
+
+  (define (iter q board)
+    (or (null? board)
+        (and (not (attacks? q (car board)))
+             (iter q (cdr board)))))
+
+  (iter kth-queen other-queens)))
+  
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
