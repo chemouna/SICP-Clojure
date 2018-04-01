d087883d25966cf0fc694c4835675326d59fe73d
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-04-01 12:47:08 +0100

    Sol to ex 2.38

---
+++ sicp-racket/ex2.38.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(define nil '())
+
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(fold-right / 1 (list 1 2 3))
+(fold-left / 1 (list 1 2 3))
+(fold-right list nil (list 1 2 3))
+(fold-left list nil (list 1 2 3))
+
+;; for fold-right and fold-left to produce the same values for any sequence op needs
+;; to be comutative 
