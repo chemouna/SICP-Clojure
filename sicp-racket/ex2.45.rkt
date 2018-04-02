219b18b770a8268a87bc54be19ce0f6f3496e939
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-04-02 09:06:48 +0100

    Ex 2.45

---
+++ sicp-racket/ex2.45.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+(require (planet "sicp.ss" ("soegaard" "sicp.plt" 2 1)))
+
+(define (square-of-four tl tr bl br)
+  (lambda (painter)
+    (let ((top (beside (tl painter) (tr painter)))
+          (bottom (beside (bl painter) (br painter))))
+      (below bottom top))))
+
+(define (split d1 d2)
+  (lambda (painter)
+    (d1 painter (d2 painter painter))))
+
+(define right-split (split beside below))
+(define up-split (split below beside))
