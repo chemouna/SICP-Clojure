c86bb8be47471521b89065837f79f348fa0be39a
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-04-02 19:08:32 +0100

    Sol to ex 2.54

--- sicp-racket/ex2.54.rkt
+++ sicp-racket/ex2.54.rkt
@@ -5,8 +5,7 @@
          (if (and (pair? (cdr a)) (pair? (cdr b)))
              (equal? (cdr a) (cdr b))
              #t))
-        ((and (not (pair? a)) (not (pair? b)) (= a b)) #t)
-        (else #f)))
+        (else (eq? a b))))
 
 (define (equal2? p1 p2)
   (cond ((and (null? p1) (null? p2)) #t)
@@ -17,11 +16,23 @@
         ((or (pair? p1) (pair? p2)) #f)
         (else (eq? p1 p2))))
 
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (equal3? list1 list2) 
+   (accumulate (lambda (x y) (and x y)) #t (map eq? list1 list2))) 
+
 (equal? '(1 2) '(1 2))
 (equal? '(1 2 3) '(1 2 3))
 (equal? 3 4)
 (equal? 4 4)
 (equal? 4 '(1 2))
+
+(display "---")
+(newline)
 
 (equal2? '(1 2) '(1 2))
 (equal2? '(1 2 3) '(1 2 3))
