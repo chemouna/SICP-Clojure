e0e3e30aea836e66d06fbc712174d730a9a28714
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-04-02 13:38:50 +0100

    Solution to ex 2.51

---
+++ sicp-racket/ex2.51.rkt
@@ -0,0 +1,56 @@
+#lang racket
+
+(require (planet "sicp.ss" ("soegaard" "sicp.plt" 2 1)))
+
+(define (transform-painter painter origin corner1 corner2)
+  (lambda (frame)
+    (let ((m (frame-coord-map frame)))
+      (let ((new-origin (m origin)))
+        (painter
+         (make-frame new-origin
+                     (vector-sub (m corner1) new-origin)
+                     (vector-sub (m corner2) new-origin)))))))
+
+(define (beside painter1 painter2)
+  (let ((split-point (make-vect 0.5 0.0)))
+    (let ((paint-left
+           (transform-painter painter1
+                              (make-vect 0.0 0.0)
+                              split-point
+                              (make-vect 0.0 1.0)))
+          (paint-right
+           (transform-painter painter2
+                              split-point
+                              (make-vect 1.0 0.0)
+                              (make-vect 0.5 1.0))))
+      (lambda (frame)
+        (paint-left frame)
+        (paint-right frame)))))
+
+
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((paint-up
+           (transform-painter painter1
+                              split-point
+                              (make-vect 1 0.5)
+                              (make-vect 0 1)))
+          (paint-down
+           (transform-painter painter2
+                              (make-vect 0 0)
+                              (make-vect 1 0)
+                              split-point)))
+      (lambda (frame)
+        (paint-up frame)
+        (paint-down frame)))))
+
+(define (shrink-to-upper-right painter)
+  (transform-painter painter
+                     (make-vect 0.5 0.5)
+                     (make-vect 1.0 0.5)
+                     (make-vect 0.5 1.0)))
+
+;(paint (beside einstein einstein))
+(paint (below einstein einstein))
+
+(paint (flip-horiz (flip-vert (rotate90 (beside (rotate90 einstein) (rotate90 einstein))))))
