9d08c3eeeefcc85feea2011c87d6570b15f4126c
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-04-03 13:22:05 +0100

    Solution for ex 2.69

--- sicp-racket/ex2.68.rkt
+++ sicp-racket/ex2.68.rkt
@@ -1,9 +1,7 @@
 #lang racket
 
 (require rackunit)
-
 (require racket/trace)
-
 
 (define (left-branch tree) (car tree))
 (define (right-branch tree) (cadr tree))
@@ -53,14 +51,13 @@
                    (make-code-tree (make-leaf 'D 1)
                                    (make-leaf 'C 1)))))
 
-;(trace decode)
-;(decode '(0 1 1 0 0 1 0 1 0 1 1 1 0) sample-tree)
-
 (define (encode-symbol symbol tree)
   (cond ((leaf? tree) (eq? symbol tree) '())
-        ((memq symbol (symbols (left-branch tree))) '(0))
-        ((memq symbol (symbols (right-branch tree))) '(1))
-        (else (error "error: symbol not found in tree")) ))
+        ((memq symbol (symbols (left-branch tree)))
+          (cons 0 (encode-symbol symbol (left-branch tree))))
+        ((memq symbol (symbols (right-branch tree)))
+         (cons 1 (encode-symbol symbol (right-branch tree))))
+        (else (error "error: symbol not found in tree"))))
 
 (define (encode message tree)
   (if (null? message)
@@ -68,8 +65,10 @@
       (append (encode-symbol (car message) tree)
               (encode (cdr message) tree))))
 
-(trace encode-symbol)
-(encode '(A D A B B C A) sample-tree)
-;(check-equal? (encode '(A D A B B C A) sample-tree) '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+(define sample-message '(A D A B B C A))
+(define sample-bits '(0 1 1 0 0 1 0 1 0 1 1 1 0))
 
-;(check-equal? (decode (encode sample-message)))
+(encode sample-message sample-tree)
+(decode sample-bits sample-tree)
+
+(check-equal? (encode sample-message sample-tree) sample-bits)
