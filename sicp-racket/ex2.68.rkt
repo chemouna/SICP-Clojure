23d19c5e32f91aa4bb47d9824eb8089f49a34ed3
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-04-03 13:12:00 +0100

    First attempt at a solution for ex 2.68

---
+++ sicp-racket/ex2.68.rkt
@@ -0,0 +1,75 @@
+#lang racket
+
+(require rackunit)
+
+(require racket/trace)
+
+
+(define (left-branch tree) (car tree))
+(define (right-branch tree) (cadr tree))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch
+               (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit -- CHOOSE-BRANCH" bit))))
+
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree (make-leaf 'D 1)
+                                   (make-leaf 'C 1)))))
+
+;(trace decode)
+;(decode '(0 1 1 0 0 1 0 1 0 1 1 1 0) sample-tree)
+
+(define (encode-symbol symbol tree)
+  (cond ((leaf? tree) (eq? symbol tree) '())
+        ((memq symbol (symbols (left-branch tree))) '(0))
+        ((memq symbol (symbols (right-branch tree))) '(1))
+        (else (error "error: symbol not found in tree")) ))
+
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
+
+(trace encode-symbol)
+(encode '(A D A B B C A) sample-tree)
+;(check-equal? (encode '(A D A B B C A) sample-tree) '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+;(check-equal? (decode (encode sample-message)))
