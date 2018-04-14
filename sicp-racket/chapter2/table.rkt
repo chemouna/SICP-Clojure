cff1bd14da6c0c428a011b6cc4ee4919255f0097
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-04-14 11:48:11 +0100

    Add apply-generic with table coercion

--- sicp-racket/chapter2/table.rkt
+++ sicp-racket/chapter2/table.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(provide put get)
+(provide put get get-coercion put-coercion)
 
 (define *op-table* (make-hash))
 (define *coercion-table* (make-hash))
@@ -9,3 +9,7 @@
   (hash-set! *op-table* (list op type) proc))
 (define (get op type)
   (hash-ref *op-table* (list op type) null))
+(define (put-coercion type1 type2 proc)
+  (hash-set! *coercion-table* (list type1 type2) proc))
+(define (get-coercion type1 type2)
+  (hash-ref *coercion-table* (list type1 type2) null))
