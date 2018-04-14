11d208f12d50b1d81125e63a2f058d15f6ecd3e9
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-04-14 09:17:13 +0100

    Add scheme number package

---
+++ sicp-racket/chapter2/scheme-number.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(require "table.rkt")
+
+(provide make-scheme-number)
+
+; install procedure 
+(define (install-scheme-number-package)
+  (put 'add '(scheme-number scheme-number)
+       (lambda (x y) (+ x y)))
+  (put 'sub '(scheme-number scheme-number)
+       (lambda (x y) (- x y)))
+  (put 'mul '(scheme-number scheme-number)
+       (lambda (x y) (* x y)))
+  (put 'div '(scheme-number scheme-number)
+       (lambda (x y) (/ x y)))
+  (put 'make 'scheme-number (lambda (x) x))
+  (put 'super-type 'scheme-number 'complex)
+  'done)
+
+; install
+(install-scheme-number-package)
+
+(define (make-scheme-number n)
+  ((get 'make 'scheme-number) n))
