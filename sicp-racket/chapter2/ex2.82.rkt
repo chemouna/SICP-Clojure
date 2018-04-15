e163e5da37bacb5dbdf60c23be8e0b227c38df26
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-04-15 16:53:58 +0100

    Fix for some issues in the implementation and adding some examples.

--- sicp-racket/chapter2/ex2.82.rkt
+++ sicp-racket/chapter2/ex2.82.rkt
@@ -2,16 +2,18 @@
 
 (require "tag.rkt")
 (require "table.rkt")
+(require "scheme-number.rkt")
+(require "complex-number.rkt")
 
 (define (coerce-to target-type other-args result)
   (if (null? other-args)
       result
       (let* ((arg (car other-args))
-            (orig-type (type-tag arg)))
+             (orig-type (type-tag arg)))
         (if (eq? target-type orig-type)
             (coerce-to target-type (cdr other-args) (append result (list arg)))
-            (let ((orig->target (get-coercion orig-type target-type)))
-              (if orig->target 
+            (let ((orig->target (get-coercion (type-tag arg) target-type)))
+              (if (and orig->target (not (null? orig->target)))
                   (coerce-to target-type (cdr other-args) (append result (list (orig->target arg))))
                   #f))))))
 
@@ -28,10 +30,10 @@
   (if (null? coercion-types)
       (error "No method for these types, and could not coerce"
                (list op (map type-tag args)))
-      (let ((coerced-args (coerce-to (car coercion-types) (cdr coercion-types) '())))
+      (let ((coerced-args (coerce-to (car coercion-types) args '())))
         (if coerced-args
             (let ((proc (get op (map type-tag coerced-args))))
-              (if proc
+              (if (and proc (not (null? proc)))
                   (apply proc op (map contents coerced-args))
                   (apply-generic-helper op (cdr coercion-types) args)))
             (apply-generic-helper op (cdr coercion-types) args)))))
@@ -40,13 +42,40 @@
 (define (apply-generic op . args)
   (let* ((type-tags (map type-tag args))
          (proc (get op type-tags)))
-         (if proc
+         (if (and proc (not (null? proc)))
              (apply proc op (map contents args))
-             (let ((uniq-types (make-uniq (type-tags))))
+             (let ((uniq-types (make-uniq type-tags)))
                (if (> (length uniq-types) 1)
                    (apply-generic-helper op uniq-types args)
                    (error "No method for this type"
                            (list op type-tags)))))))
 
+
+(define (multi-add x y z) (apply-generic 'multi-add x y z))
+
+(define op1 'multi-add)
+
+#|
+(define args1 (list (make-scheme-number 1)
+                     (make-scheme-number 2)
+                     (make-scheme-number 3)))
+
+(apply-generic-helper op1 '(scheme-number scheme-number scheme-number) args1)
+(apply-generic op1 args1)
+|# 
+
+#|
+(define args2 (list (make-scheme-number 1)
+                     (make-complex-from-real-imag 2 1)
+                     (make-scheme-number 3)))
+(apply-generic op1 args2)
+|#
+
+#|
+(define args3 (list (make-complex-from-real-imag 1 2)
+                     (make-scheme-number 3)
+                     (make-complex-from-real-imag 4 5)))
+(apply-generic op1 args3)
+|#
 
             
