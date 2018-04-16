187d12e7b34da08a57c5d34cc7eea2bed2af540d
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-04-16 23:14:18 +0100

    Add tests for tags

--- sicpclojure/src/sicp/chapter2/tag.clj
+++ sicpclojure/src/sicp/chapter2/tag.clj
@@ -9,4 +9,20 @@
     (pair? datum) (first datum)
     :else (println "Bad tagged datum: TYPE-TAG" datum)))
 
+(defn contents
+  [datum]
+  (cond
+    (number? datum) datum
+    (pair? datum) (rest datum)
+    :else (println "Bad tagged datum: CONTENTS" datum)))
+
+(defn attach-tag
+  [& args]
+  (cond
+    (= (count args) 1) (first args)
+    (= (count args) 2) (conj (second args) (first args))
+    :else (println "Bad arguments: ATTACH-TAG" args)))
+
+
+
 
--- sicpclojure/test/sicp/chapter2/tag_test.clj
+++ sicpclojure/test/sicp/chapter2/tag_test.clj
@@ -4,4 +4,15 @@
   (:use sicp.chapter2.tag))
 
 (deftest test-type-tag
-  )
+  (is (= (type-tag 2) 'scheme-number))
+  (is (= (type-tag '('real 2.13)) '(quote real))))
+
+(deftest test-contents
+  (is (= (contents 2) 2))
+  (is (= (contents '('real 2.13)) '(2.13))))
+
+(deftest test-attach-tag
+  (is (= (attach-tag 2) 2))
+  (is (= (attach-tag '('real 2.13)) '((quote real) 2.13))))
+
+(run-tests)
