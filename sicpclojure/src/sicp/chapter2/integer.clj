63cf2cab41d0f968c45bc9cc6b065edbd3f4c251
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-04-18 03:23:07 +0100

    Add integer package

---
+++ sicpclojure/src/sicp/chapter2/integer.clj
@@ -0,0 +1,40 @@
+
+(ns sicp.chapter2.integer
+  (:require [sicp.chapter2.tag :as tag]
+            [sicp.chapter2.table :as table]
+            [sicp.chapter2.rational :as rat]))
+
+(defn tag
+  [x]
+  (tag/attach-tag 'integer x))
+
+(defn integer->rational
+  [n]
+  (rat/make-rational-number n 1))
+
+(table/putt 'add '(integer integer)
+       #(tag (+ %1 %2)))
+(table/putt 'sub '(integer integer)
+            #(tag (- %1 %2)))
+
+(table/putt 'mul '(integer integer)
+            #(tag (* %1 %2)))
+
+(table/putt 'div '(integer integer)
+            #(rat/make-rational-number %1 %2))
+
+(table/putt 'equ? '(integer integer) =)
+
+(table/putt '=zero? '(integer)
+       #(= 0 %1))
+
+(table/putt 'make 'integer
+       #(if (integer? %1)
+          (tag %1)
+          (println "non-integer value" %1)))
+
+(table/put-coercion 'integer 'rational integer->rational)
+
+(defn make-integer
+  [n]
+  ((table/gett 'make 'integer) n))
--- sicpclojure/test/sicp/chapter2/raise_test.clj
+++ sicpclojure/test/sicp/chapter2/raise_test.clj
@@ -5,12 +5,12 @@
   (:require [sicp.chapter2.real :as real]
             [sicp.chapter2.rational :as rat]
             [sicp.chapter2.complex :as complex]
-            [sicp.chapter2.scheme-number :as sn]))
+            [sicp.chapter2.integer :as int]))
 
 (deftest test-raise
   (is (= (raise (real/make-real 2.12)) '(complex (rectangular (2.12 0)))))
   (is (= (raise (rat/make-rational-number 2 3)) '(real 2/3)))
   (is (= (raise (complex/make-complex-from-real-imag 2 3)) '(complex (rectangular (2 3)))))
-  (is (= (raise (sn/make-scheme-number 2))))
+  (is (= (raise (int/make-integer 2)))))
 
 (run-tests)
