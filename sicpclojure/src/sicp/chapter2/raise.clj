f83717e45c6f6e97d60bcd23c8e066a6e112f2c3
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-04-18 02:03:32 +0100

    Fix some issue in raise function and add a test.

--- sicpclojure/src/sicp/chapter2/raise.clj
+++ sicpclojure/src/sicp/chapter2/raise.clj
@@ -3,29 +3,33 @@
   (:require [sicp.chapter2.tag :as tag]
             [sicp.chapter2.table :as table]
             [clojure.tools.trace :as trace]
-            [sicp.chapter2.rational :as rat]))
+            [sicp.chapter2.rational :as rat]
+            [sicp.chapter2.real :as real]))
 
 ;(trace/trace-ns 'sicp.chapter2.raise)
-;(trace/trace-ns 'sicp.chapter2.rational)
+;(trace/trace-ns 'sicp.chapter2.table)
+;(trace/trace-ns 'sicp.chapter2.tag)
 
-(def tower-of-types '('integer 'rational 'real 'complex))
+(def tower-of-types '(integer rational real complex))
 
 (defn apply-raise
   [v types]
+  (println "apply-raise with " v types)
   (cond
     (empty? types) (println "Type not found in the tower-of-types")
     (= (tag/type-tag v) (first types))
     (if (empty? (rest types))
       v
-      (let [raise-proc (table/get-coercion (tag/type-tag v) (first types))]
-        (if raise-proc
-          (raise-proc (tag/contents v))
-          (println "No coercion found for the types "
-                   (list (tag/type-tag v) (first types))))))
-    :else (raise v (rest types))))
+     (let [raise-proc (table/get-coercion (tag/type-tag v) (second types))]
+       (if raise-proc
+           (raise-proc (tag/contents v))
+           (println "No coercion found for the types "
+                    (list (tag/type-tag v) (first types))))))
+    :else (apply-raise v (rest types))))
 
 (defn raise
   [v]
   (apply-raise v tower-of-types))
+
 
 
--- sicpclojure/test/sicp/chapter2/raise_test.clj
+++ sicpclojure/test/sicp/chapter2/raise_test.clj
@@ -1,6 +1,10 @@
 
 (ns sicp.chapter2.raise-test
-  (:use clojure.test))
+  (:use clojure.test)
+  (:use sicp.chapter2.raise)
+  (:require [sicp.chapter2.real :as real]))
 
-;(deftest test-raise
-;  )
+(deftest test-raise
+  (is (= (raise (real/make-real 2.12)) '(complex (rectangular (2.12 0))))))
+
+(run-tests)
