97ffc256332ef207239996cccfa7e15c5bb25ec2
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-04-26 08:34:44 +0100

    Tests for sparse-terms

--- sicpclojure/test/sicp/chapter2/sparse_term_test.clj
+++ sicpclojure/test/sicp/chapter2/sparse_term_test.clj
@@ -1,6 +1,7 @@
 
 (ns sicp.chapter2.sparse-term-test
   (:use [sicp.chapter2.sparse-term]
+        [sicp.chapter2.generic-term]
         [clojure.test])
   (:require [sicp.chapter2.table :as table]))
 
@@ -11,4 +12,14 @@
            '((2 5) (2 3)))))
          'sparse-terms))
 
-;; test from terms implementation
+;; test data
+(def t1 (make-term 2 5))
+(def adjoin (table/gett 'adjoin-term '(term sparse-terms)))
+(def equal? (table/gett 'equal? '(sparse-terms sparse-terms)))
+
+;; test from coeffs implementation
+
+
+;; lets test adjoin-term (privately :))
+(deftest test-adjoin-term
+  (is (equal? (adjoin t1 '()) (list t1))))
